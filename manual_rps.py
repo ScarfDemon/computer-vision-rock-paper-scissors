247e749a2d55a8a9cab56577b2162a0203c49a57
Afia Saeed <Afia@Afias-MacBook-Pro.local>
2023-01-21 21:25:51 +0000

    Fixed capital letters

--- manual_rps.py
+++ manual_rps.py
@@ -12,7 +12,7 @@ def get_user_choice():
 user_choice = get_user_choice().capitalize()
 computer_choice = get_computer_choice()
 
-def get_winner(user_choice, computer_choice):
+def get_winner(computer_choice, user_choice):
 
     def valid(user_choice):
         return (user_choice in ['Rock', 'Paper', 'Scissors']) == True #valid=True if user_choice are correct
@@ -26,8 +26,8 @@ def get_winner(user_choice, computer_choice):
         print(f"Game: {P1} vs {P2}")
         if P1 == P2:
             print("It is a tie!")
-        elif (P1=="rock" and P2=="scissors") or (P1=="paper" and P2=="rock") or (P1=="scissors" and P2=="paper"):
-            print("You win!")
+        elif (P1=="Rock" and P2=="Scissors") or (P1=="Paper" and P2=="Rock") or (P1=="Scissors" and P2=="Paper"):
+            print("You won!")
         else:
             print("You lost!")
         
