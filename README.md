004c5ff948e49272aca1e229223e7188b8b2b7ac
Afia Saeed <Afia@Afias-MacBook-Pro.local>
2023-01-21 12:08:33 +0000

    Added manual RPS game

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ In this project a trained computer vision model was produced using [Teachable-Ma
 ## Training image project model using Teachable-Machine
 Using [Teachable-Machine](https://teachablemachine.withgoogle.com/), four classes are made: Rock, Paper, Scissors, and Nothing. Each class is trained with images of me with the correct gesture (or no gesture for the Nothing class).
 
-The model was then downloaded as a Tensorflow model as a folder containing the model as  *keras_model.h5*,  and labels *labels.txt*.
+The model was then downloaded as a Tensorflow model as a folder containing the model as  `keras_model.h5`,  and labels `labels.txt`.
+
+
---
+++ manual_rps.py
@@ -0,0 +1,33 @@
+import random
+
+options = ["rock", "paper", "scissors"]
+
+def get_computer_choice():
+    computer_choice = random.choice(options)
+    return computer_choice
+
+def get_user_choice():
+    user_choice = input("Please input your RPS choice: ").lower()
+    return user_choice
+
+def roshambo():
+
+    def valid(user_choice):
+        return (user_choice in options) == True #valid=True if user_choice are correct
+    
+    global user_choice
+    global computer_choice
+    while valid(user_choice) == False:
+        print("Invalid input")
+        user_choice = input("Please input your valid RPS choice: ").lower()
+    else:
+        P1, P2 = user_choice, computer_choice
+        print(f"Game: {P1} vs {P2}")
+        winner = None
+        if P1 == P2:
+            print("Draw!")
+        elif (P1=="rock" and P2=="scissors") or (P1=="paper" and P2=="rock") or (P1=="scissors" and P2=="paper"):
+            winner = "The User" # Player 1
+        else:
+            winner = "The Computer" # Player 2
+        print(f"The winner is: {winner}" if winner!=None else "It's a tie!")
